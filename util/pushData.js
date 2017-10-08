9189ece78857682839f502a4ea4c5d1c009279ae
Johj <peter.han.9493@gmail.com>
2017-10-07 22:12:53 -0700

    Add emoji key to store data in prep for future data under message

--- util/pushData.js
+++ util/pushData.js
@@ -3,7 +3,7 @@ const database = firebase.database();
 
 module.exports = {
   pushEmojis: (message, data) => {
-    const path = `guilds/${message.guild.id}/messages/${message.createdAt.getFullYear()}-${message.createdAt.getMonth()}`;
+    const path = `guilds/${message.guild.id}/messages/${message.createdAt.getFullYear()}-${message.createdAt.getMonth()}/emojis`;
     const newPostKey = database.ref().child(path).push().key;
 
     const updates = {};
