22fb27439a5a5256c61a38dcb5f8fbe3ef6cfba2
Johj <peter.han.9493@gmail.com>
2017-11-05 19:45:07 -0800

    Remove unnecessary event handlers

--- util/loadEvents.js
+++ util/loadEvents.js
@@ -1,9 +1,7 @@
 const requireEvent = (event) => require(`../events/${event}`);
 
 module.exports = (client) => {
-  client.on('disconnect', () => requireEvent('disconnect')(client));
   client.on('message', requireEvent('message'));
   client.on('ready', () => requireEvent('ready')(client));
-  client.on('reconnecting', () => requireEvent('reconnecting')(client));
   process.on('unhandledRejection', (error) => requireEvent('unhandledRejection')(error));
 };
