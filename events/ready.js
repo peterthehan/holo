ba37bc6967bdf911857673744bd3a1e65c6cf86b
Johj <peter.han.9493@gmail.com>
2017-11-07 23:10:52 -0800

    Add playing game status

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,6 @@
 module.exports = (client) => {
+  const items = ['apples', 'wheat', 'coins', 'beer', 'my tail',];
+  const index = new Date().getDate() % items.length;
+  client.user.setGame(`with ${items[index]}.`);
   console.log(`${client.user.tag}: Ready`);
 }
